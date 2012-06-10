d2119532cc953c85b670f4093b0406a7e7fd84c9
Andy Clark <ajc97@cantab.net>
2012-06-10 16:01:20 +0100

    Extra logging and test function
    Some debug logging and test functionality to determine if this is
    working.
    
    I only care about this working for HTTPConnection and its ilk, and these
    seem to use file objects, so I need ot make sure that file objects are
    working as I expect them to be.

--- socket-throttle.py
+++ socket-throttle.py
@@ -5,9 +5,11 @@ A replacement/wraper for socket that limits the rate at which data is received.
 '''
 
 import socket
+import sys
 from time import time, sleep
 
 class ThrottledSocket(socket.socket):
+    _debug = True;
     max_bps = 1024
     
     def recv(self, *args):
@@ -15,11 +17,25 @@ class ThrottledSocket(socket.socket):
         buf = super(socket_throttle, self).recv(*args)
         end = time()
         expected_end = start + (len(buf) * 8 * self.max_bps)
+        if _debug:
+            print "Actual: received %s bytes in %s seconds: rate=%sbps" % (
+                len(buf), 
+                end - start,
+                len(buf) * 8 / (end-start) ) 
         if expected_end > end: # only sleep if recv was quicker than expected
             # assume negligable time between end and here, and that additional
             # calls to time() may end up being more lengthly than the logic
             # to determine how long to sleep for.
+            if _debug:
+                print "  Sleeping % s seconds" % (len(buf), end - start)
             sleep(end - expected_end)
+        if _debug:
+            now = time()
+            print "  Effective: received %s bytes in %s seconds: rate=%sbps" % (
+            len(buf), 
+            now - start,
+            len(buf) * 8 / (now-start) ) 
+
         
     def dup(self):
         """
@@ -30,6 +46,16 @@ class ThrottledSocket(socket.socket):
         """
         return ThrottledSocket(_sock=self._sock)
         
-# monkey patch socket to use this type of socket for everything
-socket.socket = socket.SocketType = ThrottledSocket
- 
+def patch():        
+    # monkey patch socket to use this type of socket for everything
+    socket.socket = ThrottledSocket
+    socket.SocketType = ThrottledSocket
+
+if __name__ == '__main__':
+    import httplib
+    patch()
+    h = httplib.HTTPConnection("www.python.org")
+    h.connect()
+    h.request('GET', '/')
+    response = h.getresponse()
+    print response 
